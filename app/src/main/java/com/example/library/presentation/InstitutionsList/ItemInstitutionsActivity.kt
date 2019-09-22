8f077431886c3b18e3a90013236d0e6526497162
spoker28 <skarbaev2@gmail.com>
2019-09-22 10:24:47 +0600

    Лист со списком заведений

---
+++ app/src/main/java/com/example/library/presentation/InstitutionsList/ItemInstitutionsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.library.presentation.InstitutionsList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.library.R
+
+class ItemInstitutionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_recomendation)
+    }
+}
